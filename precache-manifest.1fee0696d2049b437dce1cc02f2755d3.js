49a140c4d4b04e8326b703ed4ff3d10675fad0e3
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-07-30 01:13:47 +0200

    Updates

---
+++ precache-manifest.1fee0696d2049b437dce1cc02f2755d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269320688134c510f05bba880db89233",
+    "url": "/React_movie_app/index.html"
+  },
+  {
+    "revision": "13f0f66d5596de873a4e",
+    "url": "/React_movie_app/static/css/main.bf9403e5.chunk.css"
+  },
+  {
+    "revision": "eb0f8b8fab886869f944",
+    "url": "/React_movie_app/static/js/2.f3ed1de9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_movie_app/static/js/2.f3ed1de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f0f66d5596de873a4e",
+    "url": "/React_movie_app/static/js/main.7d6d94f6.chunk.js"
+  },
+  {
+    "revision": "fff3239b681861a5f287",
+    "url": "/React_movie_app/static/js/runtime-main.908fdc49.js"
+  }
+]);
